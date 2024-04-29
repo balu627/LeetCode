9fad07c4e13e35c24c602ff2e69242f9c55625f6
balu627 <130897199+balu627@users.noreply.github.com>
2024-04-29 18:58:33 +0530

    Time: 0 ms (100.00%), Space: 9.7 MB (95.18%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -11,15 +11,31 @@ public:
         //         }
         //     }
         // }
-        for(int i=1;i<nums.size();i++)
-        {
-            int j=i;
-            while(j>0 && nums[j-1]>nums[j] )
-            {
-                swap(nums[j-1],nums[j]);
-                j--;
-            }
+//         for(int i=1;i<nums.size();i++)
+//         {
+//             int j=i;
+//             while(j>0 && nums[j-1]>nums[j] )
+//             {
+//                 swap(nums[j-1],nums[j]);
+//                 j--;
+//             }
             
+//         }
+        for(int i = 0;i<nums.size();i++)
+        {
+            int j = i+1;
+            int mini = nums[i];
+            int idx = i;
+            while(j<nums.size())
+            {
+                if(nums[j]<mini)
+                {
+                    mini = nums[j];
+                    idx = j;
+                }
+                j++;
+            }
+            swap(nums[idx],nums[i]);
         }
     }
 };
