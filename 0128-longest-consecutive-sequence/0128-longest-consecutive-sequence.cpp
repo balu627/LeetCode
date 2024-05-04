a3604e929b3de32ecfc9b1e4954a624d1f823b0a
Balu Vamsi Krishna <130897199+balu627@users.noreply.github.com>
2024-05-04 11:21:18 +0530

    Time: 127 ms (57.08%), Space: 74 MB (48.58%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -9,27 +9,46 @@ public:
         {
             return 1;
         }
-        sort(nums.begin(),nums.end());
+        // sort(nums.begin(),nums.end());
         int maxi =1;
-        int n = nums[0];
-        int count =1;
-        for(int i=1;i<nums.size();i++)
-        {
-            if(n==nums[i])
+        // int n = nums[0];
+        // int count =1;
+        // for(int i=1;i<nums.size();i++)
+        // {
+        //     if(n==nums[i])
+        //     {
+        //         continue;
+        //     }
+        //     else if(n+1==nums[i])
+        //     {
+        //         count++;
+        //         n = nums[i];
+        //     }
+        //     else{
+        //         count =1;
+        //         n = nums[i];
+        //     }
+        //     maxi = max(maxi,count);
+        // }
+        unordered_set<int>sst;
+            for(int i=0;i<nums.size();i++)
             {
-                continue;
+                sst.insert(nums[i]);
             }
-            else if(n+1==nums[i])
+            for(auto it:sst)
             {
-                count++;
-                n = nums[i];
+                if(sst.find(it-1)==sst.end())
+                {
+                    int count =1;
+                    int x = it;
+                    while(sst.find(x+1)!=sst.end())
+                    {
+                        x = x+1;
+                        count++;
+                    }
+                    maxi = max(maxi,count);
+                }
             }
-            else{
-                count =1;
-                n = nums[i];
-            }
-            maxi = max(maxi,count);
-        }
     return maxi;
     }
 };
